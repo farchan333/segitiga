52121213bec101d92bb1c0c749ce5a9d6b7fb5a0
Farchan <mfarchan32@gmail.com>
2023-02-13 16:44:41 +0800

    segitiga Sama Kaki

--- testing.java
+++ testing.java
@@ -1,2 +1,14 @@
-soudbhsdbhsds'ds
-  
+public class testing{
+    public static void main(String[] args) {
+        int panjangSegitiga = 5;
+        for (int i = 0; i < panjangSegitiga; i++) {
+            for (int j = i; j <= panjangSegitiga; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
